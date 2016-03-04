c59361934d3f17eb1fdae76334e7ce1ee1e8feb8
vendettamit <vendett.amit@gmail.com>
2016-03-04 16:54:06 -0500

    EFPowerTool experimental branch

---
+++ src/PowerTools/Utilities/ProjectFilesPathGenUtility.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Microsoft.DbContextPackage.Utilities
+{
+    internal class ProjectFilesPathGenUtility
+    {
+        internal static void SyncDirectoryWithNamespace(string projectNamespace, string namespaceToCompare, string defaultNamespaceSuffix, string defaultDirectory, ref string syncDirectoryPath)
+        {
+            if (string.Equals(syncDirectoryPath, defaultDirectory))
+            {
+                if (!string.Equals(string.Concat(projectNamespace, ".", defaultNamespaceSuffix), namespaceToCompare, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    syncDirectoryPath = Path.Combine(syncDirectoryPath, NamesspaceToDirectoryPath(namespaceToCompare, projectNamespace));
+                }
+                else
+                {
+                    syncDirectoryPath = Path.Combine(syncDirectoryPath, defaultNamespaceSuffix);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Method to handle the cases of nested namespaces. 
+        /// </summary>
+        /// <param name="namespaceToCompare">Namespace from the host file</param>
+        /// <param name="projectNamespace">Project's default namespace</param>
+        /// <returns>Nested namespace string representing directory structure </returns>
+        private static string NamesspaceToDirectoryPath(string namespaceToCompare, string projectNamespace)
+        {
+            var namespaceArray = namespaceToCompare.Split('.');
+
+            var nestedNamespace = namespaceArray.Except(projectNamespace.Split('.')).ToArray();
+
+            if (nestedNamespace.Length <= namespaceArray.Length)
+            {
+                return string.Join("\\", nestedNamespace);
+            }
+
+            if (nestedNamespace == null && namespaceArray.Length > 1)
+            {
+                return string.Join("\\", namespaceArray);
+            }
+
+            return namespaceToCompare;
+        }
+
+    }
+}
---
+++ test/PowerTools.Test/Utilities/ProjectFilePathGenUtilityTests.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Microsoft.DbContextPackage.Utilities
+{
+    public class ProjectFilePathGenUtilityTests
+    {
+        /// <summary>
+        /// Test case to verfiy and accomodate new changes 
+        /// i.e. if efhost object have namespace changed from templates then directory should be created by that name.
+        /// </summary>
+        [Fact]
+        public void SyncDirectoryWithNamespace_NameSpaceChangeDetected_UpdatesPathHierarchy()
+        {
+            // Setup
+            string projectNamespace = "PowerTools.Tests";
+            string namespaceToCompare = "DataAccess";
+            string defaultNamespaceSuffix = "Models";
+            string defaultDirectory = "C:\\Temp\\PowerToolTests";
+            string syncDirectoryPath = "C:\\Temp\\PowerToolTests";
+
+            // Expected
+            string expectedPath = "C:\\Temp\\PowerToolTests\\DataAccess";
+
+            ProjectFilesPathGenUtility.SyncDirectoryWithNamespace(
+                                            projectNamespace,
+                                            namespaceToCompare,
+                                            defaultNamespaceSuffix,
+                                            defaultDirectory,
+                                            ref syncDirectoryPath);
+
+            Assert.Equal(expectedPath, syncDirectoryPath);
+        }
+
+        /// <summary>
+        /// Test case to verfiy and accomodate new changes 
+        /// i.e. if efhost object have namespace changed from templates then directory should be created by that name.
+        /// </summary>
+        [Fact]
+        public void SyncDirectoryWithNamespace_NewNestedNameSpaces_UpdatesPathHierarchy()
+        {
+            // Setup
+            string projectNamespace = "PowerTools.Tests";
+            string namespaceToCompare = "DAL.Models";
+            string defaultNamespaceSuffix = "Models";
+            string defaultDirectory = "C:\\Temp\\PowerToolTests";
+            string syncDirectoryPath = "C:\\Temp\\PowerToolTests";
+
+            // Expected
+            string expectedPath = "C:\\Temp\\PowerToolTests\\DAL\\Models";
+
+            ProjectFilesPathGenUtility.SyncDirectoryWithNamespace(
+                                            projectNamespace,
+                                            namespaceToCompare,
+                                            defaultNamespaceSuffix,
+                                            defaultDirectory,
+                                            ref syncDirectoryPath);
+
+            Assert.Equal(expectedPath, syncDirectoryPath);
+        }
+
+        /// <summary>
+        /// Test case to verfiy existing functionality shall not be affected by new changes.
+        /// </summary>
+        [Fact]
+        public void SyncDirectoryWithNamespace_NameSpaceAlreadyUpdated_ShouldNotUpdatePath()
+        {
+            // Setup
+            string projectNamespace = "PowerTools.Tests";
+            string namespaceToCompare = "PowerTools.Tests.Models.Mappings";
+            string defaultNamespaceSuffix = "Mappings";
+            string defaultDirectory = "C:\\Temp\\PowerToolTests";
+            string syncDirectoryPath = "C:\\Temp\\PowerToolTests";
+
+            // Expected
+            string expectedPath = "C:\\Temp\\PowerToolTests\\Models\\Mappings";
+
+            ProjectFilesPathGenUtility.SyncDirectoryWithNamespace(
+                                            projectNamespace,
+                                            namespaceToCompare,
+                                            defaultNamespaceSuffix,
+                                            defaultDirectory,
+                                            ref syncDirectoryPath);
+
+            // At second attempt no change should be detected.
+            ProjectFilesPathGenUtility.SyncDirectoryWithNamespace(
+                                projectNamespace,
+                                namespaceToCompare,
+                                defaultNamespaceSuffix,
+                                defaultDirectory,
+                                ref syncDirectoryPath);
+
+            Assert.Equal(expectedPath, syncDirectoryPath);
+        }
+
+        /// <summary>
+        /// Test case to verfiy existing functionality shall not be affected by new changes.
+        /// </summary>
+        [Fact]
+        public void SyncDirectoryWithNamespace_NoChangeInNameSpace_DirectoryShouldNotChange()
+        {
+            // Setup
+            string projectNamespace = "PowerTools.Tests";
+            string namespaceToCompare = "PowerTools.Tests.Models";
+            string defaultNamespaceSuffix = "Models";
+            string defaultDirectory = "C:\\Temp\\PowerToolTests";
+            string syncDirectoryPath = "C:\\Temp\\PowerToolTests";
+
+            // Expected
+            string expectedPath = "C:\\Temp\\PowerToolTests\\Models";
+
+            ProjectFilesPathGenUtility.SyncDirectoryWithNamespace(
+                                            projectNamespace,
+                                            namespaceToCompare,
+                                            defaultNamespaceSuffix,
+                                            defaultDirectory,
+                                            ref syncDirectoryPath);
+
+            Assert.Equal(expectedPath, syncDirectoryPath);
+        }
+    }
+}
